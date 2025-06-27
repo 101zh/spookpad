a3963279f6ff2add3c412d4a4445de768a5c098a
101zh <67253838+101zh@users.noreply.github.com>
2025-06-27 15:38:21 -0700

    edit firmware

--- firmware/keymaps/default/keymap.c
+++ firmware/keymaps/default/keymap.c
@@ -1,15 +1,34 @@
 // Copyright 2023 QMK
 // SPDX-License-Identifier: GPL-2.0-or-later
 
+#include "quantum.h"
 #include QMK_KEYBOARD_H
+#include "ws2812.h"
 
-const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = { 
-    [0] = LAYOUT(
-        KC_1,   KC_2,   KC_3,
-        KC_4,   KC_5,   KC_6,
-        KC_7,   KC_8,   KC_9
-    )
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {[0] = LAYOUT(KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9)};
+
+#ifdef RGB_MATRIX_ENABLE
+
+led_config_t g_led_config = {{
+                                 // Key Matrix to LED Index
+                                 {8, 7, 6},
+                                 {3, 4, 5},
+                                 {2, 1, 0},
+
+                             },
+                             {// LED Index to Physical Position
+                              {128, 48},
+                              {112, 48},
+                              {96, 48},
+                              {96, 32},
+                              {112, 32},
+                              {128, 32},
+                              {128, 16},
+                              {112, 16},
+                              {96, 16}},
 };
+
+#endif
 
 #ifdef OLED_ENABLE
 
@@ -20,10 +39,9 @@ oled_rotation_t oled_init_user(oled_rotation_t rotation) {
 bool oled_task_user() {
     oled_set_cursor(0, 1);
 
-    oled_write("Placeholder Text!", false);
+    oled_write("Placeholder...", false);
 
     return false;
 }
 
 #endif
-
